b0278f34d893df478a23acbf1cf499fc58c5d632
tmd22121999 <tmdtmd22121999@gmail.com>
2023-11-30 10:48:48 +0700

    change content

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,36 +1,29 @@
 <script setup>
-import { ref } from 'vue'
+import { ref } from "vue";
 
 defineProps({
   msg: String,
-})
+});
 
-const count = ref(0)
+const count = ref(0);
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <h1>Trang web đang được thực hiện</h1>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+  <div class="card text-white">
+    <!-- <button type="button" @click="count++">count is {{ count }}</button> -->
+    <p>Trang web của 99's Studio, nhóm làm game idle nhỏ tại Việt Nam</p>
+    https://tmd22121999.github.io/99s-Studio
   </div>
 
   <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
+    Mời bạn chơi thử game đầu tay do nhóm thực hiện :
+    <a href="https://ngocquang269.itch.io/the-transporter" target="_blank"
+      >The Transporter</a
+    >
   </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <!-- <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p> -->
 </template>
 
 <style scoped>
